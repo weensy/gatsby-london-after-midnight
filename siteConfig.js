f4dded31b0052f22ba428e3d9c575ee8fb491352
weensykim <hello@weensy.co>
2021-08-22 20:50:48 +0900

    Update config

--- siteConfig.js
+++ siteConfig.js
@@ -1,9 +1,10 @@
 module.exports = {
-  name: 'London After Midnight',
-  shortName: 'LAM',
-  description: 'A bold, minimal theme for Gatsby, focused on clean typography & beautiful imagery',
-  url: 'https://gatsby-lam.vaporwavy.io', // Domain of your site without prefix!
-  prefix: '/',
-  author: 'Vaporwavy',
-  twitter: ''
+  name: "London After Midnight",
+  shortName: "LAM",
+  description:
+    "A bold, minimal theme for Gatsby, focused on clean typography & beautiful imagery",
+  url: "https://gatsby-lam.aesthetic.codes", // Domain of your site without prefix!
+  prefix: "/",
+  author: "weensykim",
+  twitter: "",
 }
