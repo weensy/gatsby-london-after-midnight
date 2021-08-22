4cbecec5157e3dbd6b319af0c21e93758a210d77
weensykim <hello@weensy.co>
2021-08-22 21:13:53 +0900

    Update url

--- siteConfig.js
+++ siteConfig.js
@@ -3,7 +3,7 @@ module.exports = {
   shortName: "LAM",
   description:
     "A bold, minimal theme for Gatsby, focused on clean typography & beautiful imagery",
-  url: "https://gatsby-lam.aesthetic.codes", // Domain of your site without prefix!
+  url: "https://gatsby-lam.weensy.co", // Domain of your site without prefix!
   prefix: "/",
   author: "weensykim",
   twitter: "",
